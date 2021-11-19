55a110ac5a81d0f27278feb4d9ed2a7ff03b2522
tandpfun <dev.tandpfun@gmail.com>
2021-11-18 20:18:08 -0800

    meta tags, better description

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
     <div class="mb-8">
       <div class="max-w-5xl mx-auto mt-12">
         <div class="text-white text-center mb-8">
-          <img src="@/assets/img/logo.png" class="mx-auto h-24 mb-4" />
-          <h1 class="font-bold text-4xl">Fake Twitter NFT Avatar</h1>
-          <p class="font-semibold text-xl">Give your profile picture a hexagonal shape!</p>
+          <img src="/logo.png" class="mx-auto h-24 mb-4" />
+          <h1 class="font-bold text-4xl">Twitter NFT Avatar</h1>
+          <p class="font-semibold text-xl">Give your profile picture a hexagonal shape without an NFT!</p>
           <p class="text-md mt-1">
             Open source on <a class="text-blue-500 hover:text-blue-600 transition duration-50" href="https://github.com/tandpfun/nft-avatar" target="_blank">GitHub</a>. Made by
             <a class="text-blue-500 hover:text-blue-600 transition duration-50" href="https://twitter.com/cdngdev" target="_blank">@cdngdev</a>.
